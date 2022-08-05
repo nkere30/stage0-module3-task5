8a664d445f78ec442a1e1a1b9ae16896d3b4bae5
nkere30 <ninokereselidze30@gmail.com>
2022-08-06 03:29:42 +0400

    Completed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    static void divide(int toBeDivided) {
+        double result = toBeDivided / 5.0;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        divide(13);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void main(String[] args) {
+        convert(11.5f);
     }
+    public static void convert(float minutes) {
+        float seconds = (float) (minutes * 60);
+        System.out.println(seconds);
+    }
+
+
 }
+
+
